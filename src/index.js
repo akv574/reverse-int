47311230fe8c8a5251e3e51f5d87f4ca356a2710
akv574 <kvashurkevich@gmail.com>
2020-10-10 15:02:15 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
+    let result = String( Math.abs(n)).split("").reverse().join("");
+    result[0] == 0 ? result = result.substr(1,result.length-1) : result;
+    return Number(result)
   
 }
